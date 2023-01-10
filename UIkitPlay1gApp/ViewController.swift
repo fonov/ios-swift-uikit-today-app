d09dc8337bf7f30a4a808fa190221711f1c81dbf
Sergei Fonov <sergeufonov@gmail.com>
2023-01-10 16:49:08 +0400

    Initial Commit

---
+++ UIkitPlay1gApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitPlay1gApp
+//
+//  Created by Sergei Fonov on 10.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
