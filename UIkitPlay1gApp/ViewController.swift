96b9d1a9fdc8ca486fbfb429844915e3d33d9ec3
Sergei Fonov <sergeufonov@gmail.com>
2023-01-10 23:01:23 +0400

    create collection view

--- UIkitPlay1gApp/ViewController.swift
+++ UIkitPlay1gApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
+  typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+  var dataSource: DataSource!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
+    let listLayout = listLayout()
+    collectionView.collectionViewLayout = listLayout
+
+    let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+      let reminder = Reminder.sampleData[indexPath.item]
+      var contentConfiguration = cell.defaultContentConfiguration()
+      contentConfiguration.text = reminder.title
+      cell.contentConfiguration = contentConfiguration
+    }
+
+    dataSource = DataSource(collectionView: collectionView) { (cell: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+      return self.collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+    }
+
+    var snapshot = Snapshot()
+    snapshot.appendSections([0])
+    snapshot.appendItems(Reminder.sampleData.map { $0.title })
+    dataSource.apply(snapshot)
+
+    collectionView.dataSource = dataSource
   }
 
-
+  func listLayout() -> UICollectionViewLayout {
+    var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+    listConfiguration.showsSeparators = false
+    listConfiguration.backgroundColor = .clear
+    return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+  }
 }
 
