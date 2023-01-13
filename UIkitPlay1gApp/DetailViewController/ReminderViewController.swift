da6fafb9a0d28d8180bb9e2ed683c84e499bae67
Sergei Fonov <sergeufonov@gmail.com>
2023-01-13 12:25:07 +0400

    reminder detail

---
+++ UIkitPlay1gApp/DetailViewController/ReminderViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ReminderViewController.swift
+//  UIkitPlay1gApp
+//
+//  Created by Sergei Fonov on 12.01.23.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+  var reminder: Reminder
+
+  init(reminder: Reminder) {
+    self.reminder = reminder
+    var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+    listConfiguration.showsSeparators = false
+    let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    super.init(collectionViewLayout: listLayout)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("Always initialize ReminderViewController use init(reminder:)")
+  }
+
+  func text(for row: Row) -> String? {
+    switch row {
+    case .viewDate: return reminder.dueDate.dayText
+    case .viewNotes: return reminder.notes
+    case .viewTime: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+    case .viewTitle: return reminder.title
+    }
+  }
+}
