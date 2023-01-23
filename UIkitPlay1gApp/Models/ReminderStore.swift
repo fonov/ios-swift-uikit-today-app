733f5fd98904d76f2c62c7d5d87717a289ffb6c1
Sergei Fonov <sergeufonov@gmail.com>
2023-01-23 23:58:36 +0400

    access to reminders

--- UIkitPlay1gApp/Models/ReminderStore.swift
+++ UIkitPlay1gApp/Models/ReminderStore.swift
@@ -17,11 +17,40 @@ class ReminderStore {
     EKEventStore.authorizationStatus(for: .reminder) == .authorized
   }
 
+  func requestAccess() async throws {
+    let status = EKEventStore.authorizationStatus(for: .reminder)
+
+    switch status {
+    case .authorized:
+      return
+    case .restricted:
+      throw TodayError.assessRestricted
+    case .notDetermined:
+      let accessGranted = try await ekStore.requestAccess(to: .reminder)
+      guard accessGranted else {
+        throw TodayError.accessDenied
+      }
+    case .denied:
+      throw TodayError.accessDenied
+    @unknown default:
+      throw TodayError.unknown
+    }
+  }
+
   func readAll() async throws -> [Reminder] {
     guard isAvailable else {
       throw TodayError.accessDenied
     }
 
     let predicate = ekStore.predicateForReminders(in: nil)
+    let ekReminders = try await ekStore.fetchReminders(matching: predicate)
+    let reminders = try ekReminders.compactMap { ekReminder in
+      do {
+        return try Reminder(with: ekReminder)
+      } catch TodayError.reminderHasNoDueDate {
+        return nil
+      }
+    }
+    return reminders
   }
 }
